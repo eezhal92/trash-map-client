129eba162dc1295defd477abd8ed4d1d8efe9e05
eezhal92 <arkinthesky.69@gmail.com>
2017-09-03 12:03:30 +0800

    remove geolcation feature checking on Root component

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Route,
   BrowserRouter as Router,
@@ -11,38 +11,17 @@ import Stats from './pages/Stats';
 import Report from './pages/Report';
 import Submitted from './pages/Submitted';
 
-export default class Root extends Component {
-  constructor(props) {
-    super(props);
-    if ('geolocation' in navigator) {
-      this.state = { supported: true };
-    } else {
-      this.state = { supported: false };
-    }
-  }
-
-  render() {
-    const { supported } = this.state;
-
-    if (!supported) {
-      return (
-        <div>
-          Sorry your device is not supporting Geo Location
-        </div>
-      );
-    }
-
-    return (
-      <Router>
-        <div class="root">
-          <Route exact path="/" component={Home} />
-          <Route path="/snap" component={Snap} />
-          <Route path="/submitted" component={Submitted} />
-          <Route path="/report" component={Report} />
-          <Route path="/about" component={About} />
-          <Route path="/stats" component={Stats} />
-        </div>
-      </Router>
-    );
-  }
+export default function Root() {
+  return (
+    <Router>
+      <div class="root">
+        <Route exact path="/" component={Home} />
+        <Route path="/snap" component={Snap} />
+        <Route path="/submitted" component={Submitted} />
+        <Route path="/report" component={Report} />
+        <Route path="/about" component={About} />
+        <Route path="/stats" component={Stats} />
+      </div>
+    </Router>
+  );
 }
